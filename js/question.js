81823b98933d51398da758d3cd9adac21675d238
Nathaniel Mwabili <nathaniel.mwabz@gmail.com>
2024-12-03 02:33:16 +0300

    Second Commit

---
+++ js/question.js
@@ -0,0 +1,45 @@
+// creating an array of questions, options, and answers.
+let questions = [
+  {
+    numb: 1,
+    question: "What is a common file format for digital images?",
+    answer: ".jpg",
+    options: [".docx", ".jpg", ".mp3", ".exe"],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "Which programming language is primarily used for web development?",
+    answer: "Javascript",
+    options: ["Python", "C++", "COBOL", "Javascript"],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "Which of the following is NOT an example of an operating system?",
+    answer: "Microsoft Word",
+    options: ["Linux", "Windows", "macOS", "Microsoft Word"],
+  },
+];
